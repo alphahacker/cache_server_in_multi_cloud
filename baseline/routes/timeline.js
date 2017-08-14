e7b694514482c697db490b0338ae2020f76a54b0
한 승희 <alphahacker.han@gmail.com>
2017-08-14 22:42:23 +0900

    baseline write through : 친구수 2배 -> 3배

--- baseline/routes/timeline.js
+++ baseline/routes/timeline.js
@@ -486,7 +486,7 @@ router.get('/:userId', function(req, res, next) {
           rejected("fail to get the friendList memory in Redis");
         }
         else {
-          end = result * 2;
+          end = result * 3;
           resolved(contentIndexList);
         }
     });
